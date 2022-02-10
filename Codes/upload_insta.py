70865c9d369396b96b4a400d378909108617797f
powerful104 <p981116@naver.com>
2022-02-10 22:47:39 +0900

    upload_insta 수정

---
+++ Codes/upload_insta.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+
+"""
+upload_insta 함수
+
+매개변수 :
+    업로드 이미지
+    본문 텍스트 문자열
+
+반환값 :
+    업로드 상태 플래그 (오류 예외 방지용)
+
+기능 :
+    selenium을 활용하여 인스타에 이미지와 본문을 업로드한다.
+    업로드 계정은 @color_mixed로 한다.
+"""
+
