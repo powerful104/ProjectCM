aada3c1b3a7f4348cf994943ae395ad8bd615849
powerful104 <p981116@naver.com>
2022-02-10 22:47:12 +0900

    get_color_value 수정

--- Codes/get_color_value.py
+++ Codes/get_color_value.py
@@ -1,5 +1,17 @@
 import random
 
+"""
+get_color_value 함수
+
+매개변수 :
+
+반환값 :
+    색 정보를 가진 (0~255)범위의 세개의 정수값이 담긴 두개의 튜플 (튜플은 B, G, R의 형태로 나타낸다.)
+
+기능 :
+    gen_image 함수에서 사용될 랜덤한 RGB 정보가 담긴 튜플 두개를 반환한다.
+"""
+
 def get_color_value():
     up_color = (random.randint(0,255),random.randint(0,255),random.randint(0,255))
     down_color = (random.randint(0,255),random.randint(0,255),random.randint(0,255))
@@ -7,7 +19,3 @@ def get_color_value():
 
 a, b = get_color_value()
 print(a,b)
-
-"""
-튜플은 B, G, R의 형태로 나타낸다.
-"""
